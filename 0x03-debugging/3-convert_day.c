bb717019654d577ed9b21e029b539eb8a14d43a2
Orshfod <107032463+Orshfod@users.noreply.github.com>
2022-07-07 21:52:40 +0100

    Debugging

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -1,24 +1,52 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-* main - causes an infinite loop
-* Return: 0
+* convert_day - converts day of month to day of year, without accounting
+* for leap year
+* @month: month in number format
+* @day: day of month
+* Return: day of year
 */
 
-int main(void)
+int convert_day(int month, int day)
 {
-	int i;
-
-	printf("Infinite loop incoming :(\n");
-
-	i = 0;
-
-/*while (i < 10)*/
-/*{*/
-/*putchar(i);*/
-/*}*/
-
-	printf("Infinite loop avoided! \\o/\n");
-
-	return (0);
+    switch (month)
+    {
+        case 2:
+            day = 31 + day;
+            break;
+        case 3:
+            day = 59 + day;
+            break;
+        case 4:
+            day = 90 + day;
+            break;
+        case 5:
+            day = 120 + day;
+            break;
+        case 6:
+            day = 151 + day;
+            break;
+        case 7:
+            day = 181 + day;
+            break;
+        case 8:
+            day = 212 + day;
+            break;
+        case 9:
+            day = 243 + day;
+            break;
+        case 10:
+            day = 273 + day;
+            break;
+        case 11:
+            day = 304 + day;
+            break;
+        case 12:
+            day = 334 + day;
+            break;
+        default:
+            break;
+    }
+    return (day);
 }
